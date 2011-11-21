1bdcd32f0dda191ec29f38b996e2c852f72c02bc
porto <porto.vga@gmail.com>
2011-11-21 15:59:40 +0100

    Refactor BDConnectionHandlerImplMySQL

--- src/com/coctelmental/server/dao/BDConnectionHandlerImplMySQL.java
+++ src/com/coctelmental/server/dao/BDConnectionHandlerImplMySQL.java
@@ -9,107 +9,78 @@ import java.sql.Statement;
 
 public class BDConnectionHandlerImplMySQL implements BDConnectionHandler {
 
+	public Connection setupConnection() throws Exception {
+		try {
+			Class.forName("org.gjt.mm.mysql.Driver");
+		} catch (java.lang.ClassNotFoundException e) {
+			System.err.println("MySQL JDBC Driver not found..");
+			throw (e);
+		}
 
-    public Connection setupConnection() throws Exception
-    {
-		try
-	        {
-	            Class.forName("org.gjt.mm.mysql.Driver");
-	        }
-		catch(java.lang.ClassNotFoundException e)
-	        {
-			System.err.println("MySQL JDBC Driver no encontrado ... ");
-			throw(e);
-	        }
-	
-	    	String url="";
-		try
-	        {
-	            url="jdbc:mysql://"+ HOST +":"+ PORT +"/"+ BD_NAME;
-	            Connection con = DriverManager.getConnection(url, BD_USER, BD_USER_PASSWORD);
-	            return con;
-	        }
-		catch (java.sql.SQLException e)
-	        {
-	            System.err.println("Conexion NO establecida con " + url);
-	            throw (e);
-	        }
-    }
+		String url = "";
+		try {
+			url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + BD_NAME;
+			Connection con = DriverManager.getConnection(url, BD_USER,
+					BD_USER_PASSWORD);
+			return con;
+		} catch (java.sql.SQLException e) {
+			System.err.println("Error connecting with " + url);
+			throw (e);
+		}
+	}
 
-    public ResultSet executeQuery(Connection con, String query) throws Exception
-    {
-		try
-	        {
-	            Statement s = con.createStatement();
-	            ResultSet rs = s.executeQuery(query);
-	            
-	            return rs;
-	        }
-		catch (SQLException e)
-	        {
-	            System.err.println ("Error ejecutando la sentencia SQL: " + query );
-	            throw (e);
-	        }
-    }
+	public ResultSet executeQuery(Connection con, String query)
+			throws Exception {
+		try {
+			Statement s = con.createStatement();
+			ResultSet rs = s.executeQuery(query);
+			s.close();
 
-    public ResultSet executeQuery(Connection con, PreparedStatement ps) throws Exception
-    {
-		try
-	        {
-	            ResultSet rs = ps.executeQuery();
-	
-	            return rs;
-	        }
-		catch (SQLException e)
-	        {
-	            System.err.println ("Error ejecutando la sentencia SQL: " + ps.toString() );
-	            throw (e);
-	        }
-    }
+			return rs;
+		} catch (SQLException e) {
+			System.err.println("Error executing this SQL query: " + query);
+			throw (e);
+		}
+	}
 
+	public ResultSet executeQuery(Connection con, PreparedStatement ps)
+			throws Exception {
+		try {
+			ResultSet rs = ps.executeQuery();
 
-    @SuppressWarnings("finally")
-	public int executeUpdate(Connection con, String operation) throws Exception
-    {
-        Statement s=null;
-        int resultado=-1;
-        
-		try
-	        {
-	            s = con.createStatement();
-	            resultado=s.executeUpdate(operation);
-	            s.close();
-	
-	            return resultado;
-	        }
-		catch(SQLException e)
-	        {
-	            System.err.println ("Error ejecutando sentencia SQL: " + operation);
-	            throw (e);
-	        }
-	        finally
-	        {
-	            try
-	            {
-	                s.close();
-	            }catch (final Exception e)
-	            {
-	            // log this error
-	            }
-	            return resultado;
-	        }
-    }
+			return rs;
+		} catch (SQLException e) {
+			System.err.println("Error executing this SQL query: "
+					+ ps.toString());
+			throw (e);
+		}
+	}
 
-    public void executeUpdate(Connection con, PreparedStatement ps) throws Exception
-    {
-		try
-	        {
-	            ps.executeUpdate();
-	        }
-		catch(SQLException e)
-	        {
-	            System.err.println ("Error ejecutando sentencia SQL: " + ps.toString());
-	            throw (e);
-	        }
-	    }
+	public int executeUpdate(Connection con, String operation) throws Exception {
+		Statement s = null;
+		int result = -1;
+
+		try {
+			s = con.createStatement();
+			result = s.executeUpdate(operation);
+			s.close();
+
+			return result;
+		} catch (SQLException e) {
+			System.err.println("Error executing this SQL sentence: "
+					+ operation);
+			throw (e);
+		}
+	}
+
+	public void executeUpdate(Connection con, PreparedStatement ps)
+			throws Exception {
+		try {
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			System.err.println("Error executing this SQL sentence: "
+					+ ps.toString());
+			throw (e);
+		}
+	}
 }
